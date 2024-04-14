6d26f37d1bed578c2596591cb672ffd4762e1943
Uthsara K A R <134998461+ravin00@users.noreply.github.com>
2024-04-15 00:39:04 +0530

    changes

--- frontend/src/components/OrderUpdateForm.js
+++ frontend/src/components/OrderUpdateForm.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useParams, useNavigate } from "react-router-dom";
 
-// import './CreateOrderForm.css'
+import './UpdateForm.css';
 
 const OrderUpdateForm = () => {
   //   return (
--- frontend/src/components/OrdersAll.js
+++ frontend/src/components/OrdersAll.js
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-// import "./OrderAll.css";
+import './DataTable.css';
 
 const OrdersAll = () => {
   const [OrdersAll, setOrdersAll] = useState([]);
