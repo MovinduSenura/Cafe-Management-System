9850a8e17083bdda905824566c7d9abd9df08758
Uthsara K A R <134998461+ravin00@users.noreply.github.com>
2024-04-16 17:32:40 +0530

    footer is created for the specified pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'; // Import BrowserRouter and Routes
+import { useLocation } from 'react-router-dom'; // Import useLocation
 
 import FeedbackCreateForm from './components/FeedbackCreateForm';
 import FeedbacksAll from './components/FeedbacksAll';
@@ -14,32 +15,32 @@ import PromotionPage from './pages/PromotionPage';
 function App() {
   return (
     <div className="App">
-
-      <BrowserRouter>
-
-      <NavBar/>
-
-      <div className='pages'>
-
-        <Routes>
-
-          <Route path = '/' element = {<Home />} />
-          <Route exact path="/login" element={<CustomerLogin/>} />
-          <Route path = '/menudisplay' element = {<MenuPage />} />
-          <Route path = '/promotiondisplay' element = {<PromotionPage />} />
-
-          <Route path='/feedbackcreateform' element={<FeedbackCreateForm />}/> 
-          <Route path='/feedbackupdateform/:id' element={<UpdateForm/>}/>
-          <Route path='/allfeedbacks' element={<FeedbacksAll/>}/>
-        </Routes>
-
-      </div>
-
-      <Footer/>
-      
+      <BrowserRouter> {/* Wrap the entire component tree with BrowserRouter */}
+        <AppContent /> {/* Render the main content of the app */}
       </BrowserRouter>
-      
     </div>
+  );
+}
+
+function AppContent() {
+  const location = useLocation(); // useLocation hook
+
+  return (
+    <>
+      <NavBar />  
+      <div className='pages'>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/menudisplay" element={<MenuPage />} />
+          <Route path="/promotiondisplay" element={<PromotionPage />} />
+          <Route path="/feedbackcreateform" element={<FeedbackCreateForm />} />
+          <Route path="/feedbackupdateform/:id" element={<UpdateForm />} />
+          <Route path="/allfeedbacks" element={<FeedbacksAll />} />
+          <Route exact path="/login" element={<CustomerLogin />} />
+        </Routes>
+      </div>
+      {location.pathname !== '/login' && <Footer />} 
+    </>
   );
 }
 
